b0eec53158c3c6c55a5a203a2ce9c4494854c22c
Javier Flores <javier.floresapodaca@gmail.com>
2022-04-17 20:05:31 -0500

    toy problem - sameFrequencySquared

---
+++ tp_sameFrequencySquared.js
@@ -0,0 +1,31 @@
+/*
+Write a function called same, which accepts two arrays.
+The function should return true if every value in the array has it's corresponding value squared in the second array.
+The frequency of values must be the same.
+
+Example
+  same([1,2,3], [4,1,9]) returns true
+  same([1,2,3], [1,9]) returns false
+  same([1,2,1], [4,4,1]) returns false
+*/
+
+// Brute Force
+let same = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) return false;
+  let arr2Obj = {};
+  for (let i = 0; i < arr2.length; i++) {
+    arr2Obj[arr2[i]] = arr2Obj[arr2[i]]++ || 1;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr2Obj[Math.pow(arr1[i], 2)] >= 1) {
+      arr2Obj[arr1[i]]--;
+    } else {
+      return false;
+    }
+  }
+  return true;
+};
+
+console.log(same([1, 2, 3], [4, 1, 9]));
+console.log(same([1, 2, 3], [1, 9]));
+console.log(same([1, 2, 1], [4, 4, 1]));
